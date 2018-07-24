3d794c3c7281e95477972da3c20c4157c92a36c7
jiahui.ying <jiahui.ying@preh.cn>
2018-07-24 11:52:59 +0800

    add retrofit2 network timeout

--- app/src/main/java/com/yjh/iaer/injection/AppModule.java
+++ app/src/main/java/com/yjh/iaer/injection/AppModule.java
@@ -27,7 +27,14 @@ class AppModule {
     Webservice provideWebservice() {
 //        WifiManager wm = (WifiManager) getSystemService(WIFI_SERVICE);
 //        String ip = Formatter.formatIpAddress(wm.getConnectionInfo().getIpAddress());
+        final OkHttpClient client = new OkHttpClient.Builder()
+		.connectTimeout(20, TimeUnit.SECONDS)	
+		.readTimeout(20, TimeUnit.SECONDS)	
+		.writeTimeout(20, TimeUnit.SECONDS)	
+		.build();
+
         return new Retrofit.Builder()
+		.client(client)
                 .baseUrl(BASE_URL)
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(new LiveDataCallAdapterFactory())
